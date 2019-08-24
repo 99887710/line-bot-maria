c110694bf2b674b5c248f8150382f7a92316f80d
Jeff <Jeff.Wu@retailmerchantservices.co.uk>
2019-08-24 20:10:07 +0100

    Added bot controller

--- src/main/java/org/jw/bot/controller/BotController.java
+++ src/main/java/org/jw/bot/controller/BotController.java
@@ -1,6 +1,14 @@
 package org.jw.bot.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +17,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @RestController
 @EnableWebMvc
 @Log
+@LineMessageHandler
 public class BotController {
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
 
     @RequestMapping(path = "/bot", method = RequestMethod.POST)
-    public void postBotServer() {
+    @EventMapping
+    public void postBotServer(@RequestBody Object event) throws Exception {
         log.info("Line is posting to my bot server");
-
+        log.info("line bot event: " + event);
     }
+
+
 }
